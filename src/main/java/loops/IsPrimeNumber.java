9acb25222ed6c335d83edc551a51416a1e120b43
Shankark1990 <shankarkharade1990@gmail.com>
2025-01-22 17:12:49 +0530

    Changes

--- src/main/java/loops/IsPrimeNumber.java
+++ src/main/java/loops/IsPrimeNumber.java
@@ -1,9 +1,15 @@
 package loops;
 
+import java.util.Scanner;
+
 public class IsPrimeNumber {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
         boolean isPrimeNumber = true;
-        int number = 14;
+
 
         for (int i = 2; i < number - 1; i++) {
             if (number % i == 0) {
