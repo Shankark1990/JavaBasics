fc79c33490edde23581af2726503797d66bc3a63
Shankark1990 <shankarkharade1990@gmail.com>
2025-01-30 10:49:03 +0530

    greattest number amoung three

--- src/main/java/loops/GreatterNumber.java
+++ src/main/java/loops/GreatterNumber.java
@@ -6,25 +6,32 @@ import java.util.Scanner;
 public class GreatterNumber {
     public static void main(String[] args) {
         int num1, num2, num3;
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter first number: ");
-        num1=sc.nextInt();
+        num1 = sc.nextInt();
 
         System.out.print("Enter second number: ");
-        num2=sc.nextInt();
+        num2 = sc.nextInt();
 
         System.out.print("Enter third number: ");
-        num3=sc.nextInt();
+        num3 = sc.nextInt();
 
-        if(num1>num2 && num1>num3){
-            System.out.println("Greatter number is: "+num1+" amoung "+ num1+" "+num2+" "+num3);
-        }else if (num2>num1&& num2>num3){
-            System.out.println("Greatter number is: "+num2+" amoung "+ num1+" "+num2+" "+num3);
-        }else {
-            System.out.println("Greatter number is: "+num3+" amoung "+ num1+" "+num2+" "+num3);
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("Greatter number is: " + num1 + " amoung " + num1 + " " + num2 + " " + num3);
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("Greatter number is: " + num2 + " amoung " + num1 + " " + num2 + " " + num3);
+        } else {
+            System.out.println("Greatter number is: " + num3 + " amoung " + num1 + " " + num2 + " " + num3);
         }
 
+        // Using Ternary operators
+        int largestNumber = num3 > (num1 > num2 ? num1 : num2) ? num3 : ((num1 > num2) ? num1 : num2);
+        System.out.println("Find the greatest number using ternary operator is: "+largestNumber);
+
+        // Using Math.max() method
+
+        System.out.println("Using Math.max() method largest number is: "+Math.max(Math.max(num1,num2),num3));
 
     }
 }
