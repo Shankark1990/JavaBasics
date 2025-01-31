713f2cfaac99f105b380da7f18841c13cb34b37e
Shankark1990 <shankarkharade1990@gmail.com>
2025-01-31 16:20:53 +0530

    Minutes To Day and Year

--- src/test/java/TechPrograms/MinutesToDaysAndYear.java
+++ src/test/java/TechPrograms/MinutesToDaysAndYear.java
@@ -8,35 +8,24 @@ public class MinutesToDaysAndYear {
         int day = 24;
         int year = 365;
         int minutesInDay = 1440;
+        int inputMins=0;
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of minutes: ");
-        int inputMins = sc.nextInt();
-//        String inputMins = sc.next();
 
 
-//        if (Integer.parseInt(inputMins) % 1 == 0) {
-//
-//            int findYears = Integer.parseInt(inputMins) / (min * day * year);
-//            int findDays = Integer.parseInt(inputMins) / minutesInDay;
-//
-//            System.out.println(findYears);
-//            System.out.println(findDays);
-//
-//        } else {
-//            System.out.println("Invalid output..");
-//        }
 
         try {
+            inputMins=Integer.parseInt(sc.next());
             int findYears = inputMins / (min * day * year);
             int findDays = inputMins / minutesInDay;
 
             System.out.println("Years are: " + findYears);
             System.out.println("Days are: " + findDays);
 
-        } catch (Exception e) {
+        } catch (NumberFormatException e) {
 
-            System.out.println("Invalid input" + e.getMessage());
+            System.out.println("Invalid input");
 
         }
     }
