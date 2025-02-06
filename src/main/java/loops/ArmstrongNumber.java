0f1993fd7a865f273bf28494760f3e2700b68ea9
Shankark1990 <shankarkharade1990@gmail.com>
2025-02-06 13:40:22 +0530

    Armstrong number

---
+++ src/main/java/loops/ArmstrongNumber.java
@@ -0,0 +1,49 @@
+package loops;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+
+    public int power(int n) {
+        int count = 0;
+        while (n > 0) {
+            n = n / 10;
+            ++count;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int remainder;
+        int result = 0;
+        int number;
+        int originalNumber;
+
+        ArmstrongNumber as=new ArmstrongNumber();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        originalNumber = sc.nextInt();
+
+        number = originalNumber;
+
+        int pow = as.power(number);
+
+        while (number != 0) {
+            remainder = number % 10;
+            result = result + (int) Math.pow(remainder, pow);
+            number = number / 10;
+        }
+
+        System.out.println(result);
+
+        if (originalNumber == result)
+            System.out.println(originalNumber + " is an Armstrong number");
+        else
+            System.out.println(originalNumber + " is not an Armstrong number");
+
+    }
+
+}
