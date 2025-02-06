8079cf84eb6066df5cb1e07f1258f8da324db2cd
Shankark1990 <shankarkharade1990@gmail.com>
2025-02-06 12:03:27 +0530

    Factor numbers

---
+++ src/main/java/loops/FactorNumber.java
@@ -0,0 +1,18 @@
+package loops;
+
+import java.util.Scanner;
+
+public class FactorNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+        int number = sc.nextInt();
+        System.out.print("Factor numbers of "+number+" are: ");
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                System.out.print(" " + i);
+            }
+        }
+    }
+}
