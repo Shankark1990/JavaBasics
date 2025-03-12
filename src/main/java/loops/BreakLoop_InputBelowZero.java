7bf783d1844dd58ef78f68528b72769ce55e4c77
Shankark1990 <shankarkharade1990@gmail.com>
2025-03-12 19:25:10 +0530

    Breaking a loop

--- src/main/java/loops/BreakLoop_InputBelowZero.java
+++ src/main/java/loops/BreakLoop_InputBelowZero.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 public class BreakLoop_InputBelowZero {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter a value: ");
 
-        int num=sc.nextInt();
+
+        int num;
 
         while (true){
+
             System.out.println("loop");
+            System.out.print("Enter a value: ");
+            num=sc.nextInt();
             if(num<0)
                 break;
         }
+        System.out.println("execution stopped...");
     }
 }
