ce237791087cead2a343c3d7fa59290e00e30c1a
Shankark1990 <shankarkharade1990@gmail.com>
2025-03-13 15:57:01 +0530

    Breaking a loop

--- src/test/java/testngLabs/SauceTestNGDemo.java
+++ src/test/java/testngLabs/SauceTestNGDemo.java
@@ -1,39 +1,20 @@
-package seleniumLabs;
+package testngLabs;
 
-import modules.LoginModule;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import pageobjects.SaucePageObjects;
+import seleniumLabs.Screenshots;
+import testngLabs.TestBaseClass;
 import utils.DeleteFilesFromDirectory;
 
 import java.io.IOException;
-import java.lang.reflect.Method;
 
-public class SauceTestNGDemo {
-
-    ChromeDriver driver;
-    LoginModule loginModule;
-    Method method;
-
-    @BeforeMethod
-    public void setup(Method method) {
-        driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-        loginModule=new LoginModule(driver);
-        this.method=method;
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
-
+public class SauceTestNGDemo extends TestBaseClass {
     @Test(priority = 1, invocationCount = 3)
     public void verify_success_login() throws IOException {
         DeleteFilesFromDirectory.deleteFiles();
-        loginModule.login("standard_user","secret_sauce");
+        loginModule.login("standard_user", "secret_sauce");
         Screenshots.takeScreenshots(driver, method.getName());
 
     }
@@ -41,12 +22,12 @@ public class SauceTestNGDemo {
     /*Negative test case - enter only password and verify the error message*/
     @Test(priority = 2)
     public void verify_error_msg_empty_username_valid_password() {
-        loginModule.login("","secret_sauce");
+        loginModule.login("", "secret_sauce");
 
         String error_msg = driver.findElement(SaucePageObjects.errorMsg).getText();
         /*Hard assert will assert the result and if result is not expected
         the execution will stop and won't execute further statements*/
-        Assert.assertEquals("Epic sadface: Username1 is required",error_msg);
+        Assert.assertEquals("Epic sadface: Username1 is required", error_msg);
         Screenshots.takeScreenshots(driver, method.getName());
 
     }
@@ -55,7 +36,7 @@ public class SauceTestNGDemo {
     @Test(priority = 3)
     public void verify_error_msg_valid_username_empty_password() {
 
-        loginModule.login("standard_user","");
+        loginModule.login("standard_user", "");
 
         String error_msg = driver.findElement(SaucePageObjects.errorMsg).getText();
 
---
+++ src/test/java/testngLabs/TestBaseClass.java
@@ -0,0 +1,28 @@
+package testngLabs;
+
+import modules.LoginModule;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
+
+public class TestBaseClass {
+
+    ChromeDriver driver;
+    LoginModule loginModule;
+    Method method;
+
+    @BeforeMethod
+    public void setup(Method method) {
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        loginModule=new LoginModule(driver);
+        this.method=method;
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
