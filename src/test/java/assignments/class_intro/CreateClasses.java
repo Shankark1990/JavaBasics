7b3d14e48ddbbdf6bbf67f407feceea84d1c2ef7
Shankark1990 <shankarkharade1990@gmail.com>
2025-03-20 15:20:50 +0530

    Class intro

---
+++ src/test/java/assignments/class_intro/CreateClasses.java
@@ -0,0 +1,27 @@
+package assignments.class_intro;
+
+public class CreateClasses {
+}
+
+class Human{
+    
+}
+class MobilePhone{
+
+}
+class Desktop{
+
+}
+class College {
+
+}
+class TestSuite{
+
+}
+class TestCase{
+
+}
+class TestStep{
+
+}
+
