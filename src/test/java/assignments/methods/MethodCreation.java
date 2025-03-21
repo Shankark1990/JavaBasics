5406ebe72ca5fb35e2e1b1f86c5bf7d9aacd8a46
Shankark1990 <shankarkharade1990@gmail.com>
2025-03-21 11:17:03 +0530

    method

---
+++ src/test/java/assignments/methods/MethodCreation.java
@@ -0,0 +1,23 @@
+package assignments.methods;
+
+public class MethodCreation {
+
+    /*This is non static method*/
+    public int show(int a, int b) {
+        System.out.println("This is non-static method");
+        return 0;
+
+    }
+
+    /*This is static method*/
+    public static void demo() {
+        System.out.println("static method");
+    }
+
+    public static void main(String[] args) {
+        MethodCreation m1 = new MethodCreation();
+        m1.show(12,20);
+        demo();
+
+    }
+}
