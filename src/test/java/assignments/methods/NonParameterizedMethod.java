028d598c9937b51e95de4fc741d875c02da32c7a
Shankark1990 <shankarkharade1990@gmail.com>
2025-03-21 22:23:09 +0530

    Non parameterized.

---
+++ src/test/java/assignments/methods/NonParameterizedMethod.java
@@ -0,0 +1,17 @@
+package assignments.methods;
+
+public class NonParameterizedMethod {
+    public void test(){
+        System.out.println("Non-static, non parameterized method");
+    }
+
+    public static void test1(){
+        System.out.println("Static method, non parameterized");
+    }
+
+    public static void main(String[] args) {
+        NonParameterizedMethod np=new NonParameterizedMethod();
+        np.test();
+        test1();
+    }
+}
