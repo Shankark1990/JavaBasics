41ada1c88f51d06fd7893e13e436a8b9b39dbfe6
Shankark1990 <shankarkharade1990@gmail.com>
2025-04-01 12:33:12 +0530

    test

--- src/main/java/string/StringCompare.java
+++ src/main/java/string/StringCompare.java
@@ -10,7 +10,7 @@ public class StringCompare {
 
 //        System.out.println(s3==s4);
 
-        System.out.println("s1 == s2 " + (s1 == s2) );
+        System.out.println("s1 == s2: " + (s1 == s2) );
 
         Thread t1=Thread.currentThread();
         System.out.println(t1);
---
+++ src/test/java/assignments/methods/CallMethodoutsiteMethod.java
@@ -0,0 +1,15 @@
+package assignments.methods;
+
+public class CallMethodoutsiteMethod {
+
+  static int k=m1();
+
+   public static int m1(){
+       System.out.println("m1");
+       return 0;
+   }
+
+    public static void main(String[] args) {
+        System.out.println(k);
+    }
+}
